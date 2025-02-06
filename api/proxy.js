38d2bac636777154f0d60a9ac8100096f520e20b
Serdiox <havox004@gmail.com>
2025-02-06 23:29:18 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,14 @@
 export default async function handler(req, res) {
-    const response = await fetch("https://a766-121-200-55-39.ngrok-free.app");
-    const body = await response.text();
-    
-    res.setHeader("Content-Type", "text/html");
-    res.send(body);
+    try {
+        const response = await fetch("https://a766-121-200-55-39.ngrok-free.app");
+        const body = await response.text();
+
+        res.setHeader("Content-Type", "text/html");
+        res.setHeader("Access-Control-Allow-Origin", "*"); 
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+
+        res.status(200).send(body);
+    } catch (error) {
+        res.status(500).send("Error fetching content");
+    }
 }
